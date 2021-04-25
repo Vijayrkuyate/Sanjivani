b08d94615efde94e6aa8e011badb375765e04ad9
Vijayrkuyate <83130333+Vijayrkuyate@users.noreply.github.com>
2021-04-25 19:36:58 +0530

    Loader changes

--- SanjivaniERP/Controllers/DirectorController.cs
+++ SanjivaniERP/Controllers/DirectorController.cs
@@ -10,7 +10,7 @@ using OfficeOpenXml;
 using OfficeOpenXml.Table;
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
-using IronPdf;
+
 
 namespace SanjivaniERP.Controllers
 {
@@ -985,35 +985,8 @@ namespace SanjivaniERP.Controllers
         }
         public ActionResult ExportPDF()
         {
-           // return new Rotativa.ActionAsPdf("GetPersons");
-            Console.WriteLine("Hold on tight!");
-
-            var htmlToPdf = new HtmlToPdf();  // new instance of HtmlToPdf
-
-            //html to pdf
-            //html to turn into pdf
-            var html = @"<h1>Hello World!</h1><br><p>This is IronPdf.</p>";
-
-            // turn html to pdf
-            var pdf = htmlToPdf.RenderHtmlAsPdf(html);
-
-            // save resulting pdf into file
-            pdf.SaveAs(Server.MapPath("~/Content")); 
-
-            //url to pdf
-            // uri of the page to turn into pdf
-            var uri = new Uri("http://www.google.com/ncr");
-
-            // turn page into pdf
-            pdf = htmlToPdf.RenderUrlAsPdf(uri);
-
-            // save resulting pdf into file
-            pdf.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "UrlToPdf.Pdf"));
-
-            Console.WriteLine("Done. Please find results under '{0}' directory.", Directory.GetCurrentDirectory());
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey();
             return View();
+         
         }
         /// <summary>
         /// /Employeee
